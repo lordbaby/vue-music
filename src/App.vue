5a79e1d91dd4cb65bf59142b6e47a7a6e0151be9
lordbaby <676708415@qq.com>
2017-12-13 21:02:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <tab />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Tab from '@/components/tab/tab'
+export default {
+  name: 'app',
+  components: {
+    Tab
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/rank/rank.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Rank</div>
+</template>
+<script>
+    export default {
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
